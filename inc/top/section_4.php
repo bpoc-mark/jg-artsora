6dfc598afa629e4492abfbec1e996254711ebe09
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-08 16:46:49 +0800

    pushed sect_4

---
+++ inc/top/section_4.php
@@ -0,0 +1,48 @@
+<section class="sect_4" id="sect_4">
+    <div class="l-wrap">
+        
+        <div class="sect_2__header">
+            <p class="title">お客様の声</p>
+            <p class="text">VOICES</p>
+        </div>
+
+        <div class="body_content">
+            
+            
+
+
+            <div class="box_1">
+                <div class="l-wrap">
+                <div class="left">
+                    <p class="title">四日市市　N様</p>
+                    <p class="text">
+                    まず、日々の疲れの取れ方が全然違います。仕事から帰って来て、疲れて寝ることもしばしば。起床時のスッキリ具合には驚きました。
+                    快適さはもちろん、お気に入りの場所もたくさんあります。気持ちのいい吹き抜けリビングにいる時間はついついリラックスし過ぎてしまいますね。無垢材を使うことや、2階の廊下からリビングを見下ろせるようにする、水回りをひとまとめにする、御在所山を眺められるようにするなど、間取りに対するこだわりもたくさんありましたが、アート・宙のおかげで予算内に収めつつ、理想の住まいができました。
+                    </p>
+                </div>
+
+                <div class="right">
+                    <img src="<?php echo get_template_directory_uri() . '/release/image/sect_4/img1.png' ?>" alt="">
+                </div>
+                </div>
+            </div>
+
+
+            <div class="box_2">
+                <div class="left">
+                    <img src="<?php echo get_template_directory_uri() . '/release/image/sect_4/img2.png' ?>" alt="">
+                </div>
+
+                <div class="right">
+                    <p class="title">四日市市　H様</p>
+                    <p class="text">
+                    どこかに出かけても、「早く家に帰りたい」と思うくらい、我が家が居心地の良いです。この空間が好きなんです。アート・宙は性能・品質も満足のいくもので、寒さの厳しい冬でも薪ストーブを朝3、4時間つけているだけで快適です。
+                    アート・宙の小椋さんに任せれば大丈夫！小椋さんの家づくりに対する情熱や、設計士になった経緯などを聞いて「この人にお願いしよう」という気持ちになりました。その決断は間違っていなかったな、と感じています。
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
