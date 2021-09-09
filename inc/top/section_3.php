9c05b53d22b3c8d366cb4e8c5694ef6ac8a3ea77
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-09 19:22:51 +0800

    merge sect_3

---
+++ inc/top/section_3.php
@@ -0,0 +1,44 @@
+<section class="sect_3" id="sect_3">
+    <div class="l-wrap">
+
+        <div class="sect_3__header">
+            <p class="title">施工事例</p>
+            <p class="text">WORKS</p>
+        </div>
+
+        <ul class="sect_3__slider">
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img1.png' ?>" alt="">
+                <p class="text">タイトルタイトルタイトルタイトル</p>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img2.png' ?>" alt="">
+                <p class="text">薪ストーブと回遊できる土間のある家</p>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img3.png' ?>" alt="">
+                <p class="text">タイトルタイトルタイトルタイトル</p>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img1.png' ?>" alt="">
+                <p class="text">タイトルタイトルタイトルタイトル</p>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img2.png' ?>" alt="">
+                <p class="text">薪ストーブと回遊できる土間のある家</p>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img3.png' ?>" alt="">
+                <p class="text">タイトルタイトルタイトルタイトル</p>
+            </li>
+        </ul>
+
+        <div class="foot">
+            <p class="text">
+                MORE
+            </p>
+            <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/line.png' ?>" alt="">
+        </div>
+
+    </div>
+</section>
--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -91,3 +91,40 @@ $('.slider').slick({
       // instead of a settings object
     ]
   });
+
+
+
+
+  $('.sect_3__slider').slick({
+    dots: false,
+    infinite: false,
+    autoplay: false,
+    swipe: false,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      }
+    }, {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        arrows: true,
+        swipe: true,
+      }
+    } // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+  });
