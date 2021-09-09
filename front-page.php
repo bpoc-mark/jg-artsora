671a628b376d6c7d4644b8f1d022996846daaf40
arsua-greg <gregarsua@bpoc.co.jp>
2021-09-09 09:27:48 +0800

    add contact form

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
 <?php include get_template_directory() . '/inc/top/section_1.php'; ?>
-<?php include get_template_directory() . '/inc/top/section_5.php'; ?>
+<?php include get_template_directory() . '/inc/top/section_6.php'; ?>
 <?php get_footer(); ?>
