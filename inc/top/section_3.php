b645498115ecdc8ff22d719819d5b9afa8b2e430
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-14 11:18:33 +0800

    corrections 140,141,145,146,149

--- inc/top/section_3.php
+++ inc/top/section_3.php
@@ -9,27 +9,27 @@
         <ul class="sect_3__slider">
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img1.png' ?>" alt="">
-                <p class="text">タイトルタイトルタイトルタイトル</p>
+                <p class="text"> 色使いが多彩な、三角屋根の木の家</p>
             </li>
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img2.png' ?>" alt="">
-                <p class="text">薪ストーブと回遊できる土間のある家</p>
+                <p class="text">縦と横に広がる薪ストーブのある家</p>
             </li>
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img3.png' ?>" alt="">
-                <p class="text">タイトルタイトルタイトルタイトル</p>
+                <p class="text">シンプルな木の家</p>
             </li>
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img1.png' ?>" alt="">
-                <p class="text">タイトルタイトルタイトルタイトル</p>
+                <p class="text"> 色使いが多彩な、三角屋根の木の家</p>
             </li>
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img2.png' ?>" alt="">
-                <p class="text">薪ストーブと回遊できる土間のある家</p>
+                <p class="text">縦と横に広がる薪ストーブのある家</p>
             </li>
             <li>
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img3.png' ?>" alt="">
-                <p class="text">タイトルタイトルタイトルタイトル</p>
+                <p class="text">シンプルな木の家</p>
             </li>
         </ul>
 
