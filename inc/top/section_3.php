5f4be87edcdda3c6aafb2b135e900b2a020cb29f
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-16 15:50:31 +0800

    edit carousel arrow

--- inc/top/section_3.php
+++ inc/top/section_3.php
@@ -6,43 +6,43 @@
             <p class="text">WORKS</p>
         </div>
 
-        <ul class="sect_3__slider">
-            <li>
+        <div class="sect_3__slider">
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img1.png' ?>" alt="">
                 <p class="text"> 赤い差し掛け屋根がアクセントの平屋</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img2.png' ?>" alt="">
                 <p class="text">造作の建具と家具を配したリビングダイニング</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img3.png' ?>" alt="">
                 <p class="text">インテリアが空間のアクセントに、ナチュラルLDK</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img4.jpeg' ?>" alt="">
                 <p class="text"> 色使いが多彩な三角屋根の木の家</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img5.jpeg' ?>" alt="">
                 <p class="text">自然を取り込んだ心地よいモダンなLDK</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img6.jpeg' ?>" alt="">
                 <p class="text">造作キッチン背面収納とアイランドキッチン</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img7.jpeg' ?>" alt="">
                 <p class="text"> ダークブラウンの無垢材が落ち着いた印象のLDK</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img8.jpeg' ?>" alt="">
                 <p class="text">ガラスを配した玄関ドアが◎広々玄関</p>
-            </li>
-            <li>
+            </div>
+            <div class="item">
                 <img src="<?php echo get_template_directory_uri() . '/release/image/sect_3/img9.jpeg' ?>" alt="">
                 <p class="text">開放感たっぷりの吹き抜けリビング</p>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </section>
