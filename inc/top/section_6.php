75fc926fc89501ad425c2aa9106ed5df313ac0d2
arsua-greg <gregarsua@bpoc.co.jp>
2021-09-16 20:54:02 +0800

    drop down

--- inc/top/section_6.php
+++ inc/top/section_6.php
@@ -6,7 +6,7 @@
                 <p class="sub">CONTACT</p>
             </div>
             <div class="form_container">
-                <?php echo do_shortcode('[contact-form-7 id="25" title="Contact form 1"]'); ?>
+                <?php echo do_shortcode('[contact-form-7 id="12" title="Contact Form"]'); ?>
             </div>
         </div>
     </div>
