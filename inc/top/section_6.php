7ca7f00a840e0c09be9400bb66926bfa0a4a3485
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-20 17:14:20 +0800

    add text between form header

--- inc/top/section_6.php
+++ inc/top/section_6.php
@@ -3,7 +3,13 @@
         <div class="l-wrap">
             <div class="ttl_cont">
                 <p class="title">資料請求・来場予約</p>
+                
                 <p class="sub">CONTACT</p>
+                <p class="informational_text">
+                展示場見学、アート・宙の施工事例集、コンセプトブックをご希望の方は、以下フォームよりご連絡くださいませ。
+展示場見学の方は、「お客様からの伝言欄」にご来場希望日時のご記入をお願いいたします。
+お電話でも受付ております。<a href="tel:0120-07-0817">0120-07-0817</a> (火水定休)
+                </p>
             </div>
             <div class="form_container">
                 <?php echo do_shortcode('[contact-form-7 id="12" title="Contact Form"]'); ?>
