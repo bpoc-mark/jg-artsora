d6dc42b9de8593370a349c419c36d33356dbf06b
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-09-20 15:01:22 +0800

    add zipcode

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         </div>
         </main>
 
+        <script src="https://ajaxzip3.github.io/ajaxzip3.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -12,6 +13,10 @@
           AOS.init({
             once: true
           });
+
+          $('.zipInput').on('keyup', function() {
+                AjaxZip3.zip2addr('zip_s', '', 'address_s', 'address_s');
+            });
         </script>
 
         <?php wp_footer(); ?>
