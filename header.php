89cabd49467c4cd9c48d9b9ed7fde070a7deb026
arsua-greg <gregarsua@bpoc.co.jp>
2021-09-20 12:12:41 +0800

    add og image and apple icon

--- header.php
+++ header.php
@@ -7,7 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
     <title><?php bloginfo('title'); ?></title>
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="<?php echo get_site_url(); ?>">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/release/image/artsora-02.png">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="" />
+    <meta property="fb:app_id" content="">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/release/image/artsora-01.png" />
     <link rel="shortcut icon" type="image/jpg" href="<?php echo get_template_directory_uri(); ?>/release/image/artsora-01.png" />
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/release/css/style.css">
